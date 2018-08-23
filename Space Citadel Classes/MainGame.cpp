17b783a8ca1127fb2623b9a7e5fe92183a997075
Elias Perez <37880438+Elionidas@users.noreply.github.com>
2018-08-23 11:09:22 -0500

    Update MainGame.cpp
    added class selection and starter item generation

--- Space Citadel Classes/MainGame.cpp
+++ Space Citadel Classes/MainGame.cpp
@@ -15,11 +15,6 @@ void typeIt(string it, int x, bool y);
 
 int main()
 {
-	//here are the three player initializations, so the user can select which to play as
-	InterStellar_Idiot chosenOne;
-	Space_Ranger chosenTwo;
-	CloudTrooper chosenThree;
-	
 	//hold the name to pass the name
 	int selection;
 	string name;
@@ -27,7 +22,7 @@ int main()
 	//begin the story
 	string it = "The year is 4018. The human race has crossed into the final frontier, and discovered that space wasn't very lonely.";
 	typeIt(it, 75, 1);
-	it = "Indeed, it was populated with all manner of beings. One such was the famous Cap'n Slap. A renowned space pirate, he had quit his less - than noble career in favor of a better one - being a game host. You are…… who are you again ?";
+	it = "Indeed, it was populated with all manner of beings. One such was the famous Cap'n Slap. A renowned space pirate, he had quit his less - than noble career in favor of a better one - being a game host. You areâ€¦â€¦ who are you again ?";
 	typeIt(it, 75, 1);
 	cout << "_SELECT NAME_" << endl;
 	cout << "1.) Greasy McFry" << endl;
@@ -71,7 +66,7 @@ int main()
 		break;
 	}
 	
-	it = "Oh. That’s….. nice? Either way, welcome to Cap’n Slap’s Space Citadel, the most popular game show in 4 quadrants! Cap’n Slap has invited you to this somewhat lovely place to try and earn fame and glory! And maybe not die!";
+	it = "Oh. Thatâ€™sâ€¦.. nice? Either way, welcome to Capâ€™n Slapâ€™s Space Citadel, the most popular game show in 4 quadrants! Capâ€™n Slap has invited you to this somewhat lovely place to try and earn fame and glory! And maybe not die!";
 	typeIt(it, 75, 1);
 	cout << "_SELECT CLASS_" << endl;
 	cout << "1.) Inter-Galactic Idiot" << endl;
@@ -79,23 +74,72 @@ int main()
 	cout << "3.) CloudTrooper" << endl;
 	cin >> selection;
 
+	//player selection done right
+	Player Selected;
+
 	//now create the actual player with their name
 	switch(selection)
 	{
 	case 1:
-		chosenOne.Name = name;
+		Selected.Name = name;
 		break;
 	case 2:
-		chosenTwo.Name = name;
+		Selected.Name = name;
 		break;
 	case 3:
-		chosenThree.Name = name;
+		Selected.Name = name;
+		break;
+	default:
+		cout << "Your lack of choosing one of three numbers is.... disturbing. We'll just go with Inter-Stellar Idiot " << endl;
+		Selected.Name = name;
+		selection = 1;
 		break;
 	}
 
-	//add the starting items
+	//create the starting items
+	Stick stickyStick;
+	RedPotion HealthUp;
+	Bucket bucket;
+
+	//player creation yo
+	if (selection == 1)
+	{
+		InterStellar_Idiot Player1;
+		Player1.Name = name;
+		
+		//add the starting items
+		Player1.Backpack[1] = stickyStick;
+		Player1.Backpack[2] = HealthUp;
+		Player1.Backpack[3] = bucket;
+	}
+	else if (selection == 2)
+	{
+		Space_Ranger Player1;
+		Player1.Name = name;
+
+		//add the starting items
+		Player1.Backpack[1] = stickyStick;
+		Player1.Backpack[2] = HealthUp;
+		Player1.Backpack[3] = bucket;
+	}
+	else if (selection == 3)
+	{
+		CloudTrooper Player1;
+		Player1.Name = name;
+
+		//add the starting items
+		Player1.Backpack[1] = stickyStick;
+		Player1.Backpack[2] = HealthUp;
+		Player1.Backpack[3] = bucket;
+	}
 	
-	
+	it = "-Capâ€™n Slap has arrived to bid you greetings! Or farewell! Or both!-";
+	typeIt(it, 75, 1);
+	it = "AAAARRRGH, oh god how do they do that? As you know Iâ€™m Capâ€™n Slap. Iâ€¦ wonâ€™t explain how I got that title. Just know I have several Space-Restraining orders. Anyway, welcome to the Space Citadel. Iâ€™ll be watching your progress from the Space-Television. Good Luck!";
+	typeIt(it, 75, 1);
+	cout << "_PRESS ENTER TO....... ENTER THE DUNGEON_" << endl;
+
+
 	return 0;
 }
 
