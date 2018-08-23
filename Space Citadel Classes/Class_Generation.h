5db40126178cb4886f74fcccf5b6db1a90e3b722
Elias Perez <37880438+Elionidas@users.noreply.github.com>
2018-08-23 13:56:51 -0500

    added class_generation.h
    automatic generation of game resources

---
+++ Space Citadel Classes/Class_Generation.h
@@ -0,0 +1,60 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+#include <chrono>
+#include <thread>
+#include "Player.h"
+#include "player_sub.h"
+#include "Items.h"
+#include "item_sub.h"
+#include "Enemies.h"
+#include "enemy_sub.h"
+using namespace std;
+
+//player selection done right
+Player Selected;
+
+//create the starting items
+Stick stickyStick;
+RedPotion HealthUp;
+Bucket bucket;
+
+//generating all items
+RedPotion HealthUp;
+GreenPotion PoisonCup;
+Sword IronSword;
+Cutlass Swashbuckler;
+Halberd pointyBoi;
+HasselSawedOff hehe;
+Glock rockOut;
+Scimitar slashySlash;
+CanLid TheLid;
+ChainMale LinkNotZelda;
+MedKit Bandages;
+Apple GrannySmith;
+Banana nana;
+PlateMail instantShip;
+
+//Generate Enemies, up to three duplicates of each enemy type have been generated
+SpaceGoblin Goblin1;
+SpaceGoblin Goblin2;
+SpaceGoblin Goblin3;
+SpaceHobGoblin HobbyGobby1;
+SpaceHobGoblin HobbyGobby2;
+SpaceHobGoblin HobbyGobby3;
+SpaceSquirrel Squirrel1;
+SpaceSquirrel Squirrel2;
+SpaceSquirrel Squirrel3;
+SpaceBandit Bandit1;
+SpaceBandit Bandit2;
+SpaceBandit Bandit3;
+BritishRobot Durlec1;
+BritishRobot Durlec2;
+BritishRobot Durlec3;
+Spider SpideyBoi1;
+Spider SpideyBoi2;
+Spider SpideyBoi3;
+Arachne BiteyBoi1;
+Arachne BiteyBoi2;
+Arachne BiteyBoi3;
