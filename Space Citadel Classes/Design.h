a5e7b3255a31319bcf1c405dc0667868802c3b8c
Elias Perez <37880438+Elionidas@users.noreply.github.com>
2018-08-27 11:29:41 -0500

    added intro, generation, and design

---
+++ Space Citadel Classes/Design.h
@@ -0,0 +1,63 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include "Items.h"
+using namespace std;
+
+class Design {
+public:
+	string Name = ""; //name of the weapon, calling a different weapon calls a different design
+
+	//based on the name of the weapon, display something different on burright's screen
+	void showWeapon(Weapon wp) {
+		//get ready for the mother of all if statements
+		if (wp.name == "Iron Blade")
+		{
+			weaponR1 = "         />_______________________";
+			weaponR2 = " [#######[]_______________________>";
+			weaponR3 = "         \\>";
+			weaponName = "Iron Blade";  // <-- 30 CHARS TOTAL FOR NAME AND DMG COMBINED
+			weaponDamage = 7;  // <--------------
+		}
+		else if (wp.name == "Sticky Stick")
+		{
+			weaponR1 = "                 ";
+			weaponR2 = "            ===============---   ";
+			weaponR3 = "    o======//         \\";
+			weaponName = "Sticky stick";
+			weaponDamage = 2;
+		}
+		else if (wp.name == "Rusty Metal Probe")
+		{
+			weaponR1 = "            \\         ";
+			weaponR2 = "  O==========E>>>                 ";
+			weaponR3 = "            /          ";
+			weaponName = "Rusty metal probe";
+			weaponDamage = 10;
+		}
+		else if (wp.name == "Slashy-Boi")
+		{
+			weaponR1 = "         />__________";
+			weaponR2 = " [#######[]________ _ _ _ _ ------->";
+			weaponR3 = "         \\>";
+			weaponName = "Slashy-Boi";  // <-- 30 CHARS TOTAL FOR NAME AND DMG COMBINED
+			weaponDamage = 20;  // <--------------
+		}
+		else if (wp.name == "Tricera- Glock")
+		{
+			weaponR1 = "'===='";
+			weaponR2 = "/ /'";
+			weaponR3 = "~";
+			weaponName = "Tricera- Glock";  // <-- 30 CHARS TOTAL FOR NAME AND DMG COMBINED
+			weaponDamage = 10;  // <--------------
+		}
+		else if (wp.name == "Hassel Sawed-Off")
+		{
+			weaponR1 = "'___________";
+			weaponR2 = "/ /'________00'";
+			weaponR3 = "~";
+			weaponName = "Hassel Sawed-Off";  // <-- 30 CHARS TOTAL FOR NAME AND DMG COMBINED
+			weaponDamage = 30;  // <--------------
+		}
+	}
+};
---
+++ Space Citadel Classes/intro.h
@@ -0,0 +1,163 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+#include <chrono>
+#include <thread>
+#include <conio.h>
+#include <Windows.h>
+#include <cstdlib>
+#include "Player.h"
+#include "player_sub.h"
+#include "Items.h"
+#include "item_sub.h"
+#include "Enemies.h"
+#include "enemy_sub.h"
+#include "Class_Generation.h"
+using namespace std;
+
+void typeIt(string it, int x, bool y);
+int Intro();
+
+int Intro()
+{
+	//hold the name to pass the name
+	int selection;
+	string name;
+
+	//begin the story
+	string year = "The year is 4018. The human race has crossed into the final frontier, and discovered that space wasn't very lonely.";
+	typeIt(year, 45, 1);
+	string indeed = "Indeed, it was populated with all manner of beings. One such was the famous Cap'n Slap. A renowned space pirate, he had quit his less - than noble career in favor of a better one - being a game host. You are... who are you again?";
+	typeIt(indeed, 45, 1);
+	cout << "_SELECT NAME_" << endl;
+	cout << "1.) Greasy McFry" << endl;
+	cout << "2.) XxTotallyLegit420xX" << endl;
+	cout << "3.) I Love the Space Force" << endl;
+	cin >> selection;
+
+	switch (selection)
+	{
+	case 1:
+		name = "Greasy McFry";
+		break;
+	case 2:
+		name = "XxTotallyLegit420xX";
+		break;
+	case 3:
+		name = "I Love the Space Force";
+		break;
+	default:
+		cout << "since you cant pick between 1, 2, or 3, you get to be named, 'Intergalactic Idiot'" << endl;
+		name = "Intergalactic Idiot";
+		break;
+	}
+
+	cout << "Ah, " << name << " . What is your story?" << endl;
+	cout << "_SELECT USELESS BACKGROUND_" << endl;
+	cout << "1.) I am a Veteran of the United States Space Force." << endl;
+	cout << "2.) I am a masked Vigilante. The protector of Go- I mean Space Detroit." << endl;
+	cout << "3.) I am a random person with some boring backstory." << endl;
+	cin >> selection;
+
+	switch (selection)
+	{
+	case 1:
+		break;
+	case 2:
+		break;
+	case 3: //suprise! no one cares about the background!
+		break;
+	default:
+		break;
+	}
+
+	string oh = "Oh. That's.... nice? Either way, welcome to Cap'n Slap's Space Citadel, the most popular game show in 4 quadrants! Cap'n Slap has invited you to this somewhat lovely place to try and earn fame and glory! And maybe not die!";
+	typeIt(oh, 45, 1);
+	cout << "_SELECT CLASS_" << endl;
+	cout << "1.) Inter-Galactic Idiot" << endl;
+	cout << "2.) Space-Ranger" << endl;
+	cout << "3.) CloudTrooper" << endl;
+	selection = 0;
+	cin >> selection;
+
+	//now create the actual player with their name
+	switch (selection)
+	{
+	case 1:
+		Selected.Name = name;
+		break;
+	case 2:
+		Selected.Name = name;
+		break;
+	case 3:
+		Selected.Name = name;
+		break;
+	default:
+		cout << "Your lack of choosing one of three numbers is.... disturbing. We'll just go with Inter-Stellar Idiot " << endl;
+		Selected.Name = name;
+		selection = 1;
+		break;
+	}
+
+	//player creation yo
+	if (selection == 1)
+	{
+		InterStellar_Idiot Player1;
+		Player1.Name = name;
+
+		//add the starting items
+		Player1.Backpack[0] = stickyStick;
+		Player1.Backpack[1] = HealthUp;
+		Player1.Backpack[2] = bucket;
+	}
+	else if (selection == 2)
+	{
+		Space_Ranger Player1;
+		Player1.Name = name;
+
+		//add the starting items
+		Player1.Backpack[0] = stickyStick;
+		Player1.Backpack[1] = HealthUp;
+		Player1.Backpack[2] = bucket;
+	}
+	else if (selection == 3)
+	{
+		CloudTrooper Player1;
+		Player1.Name = name;
+
+		//add the starting items
+		Player1.Backpack[0] = stickyStick;
+		Player1.Backpack[1] = HealthUp;
+		Player1.Backpack[2] = bucket;
+	}
+
+	string cap = "-Cap'n Slap has arrived to bid you greetings! Or farewell! Or both!-";
+	typeIt(cap, 45, 1);
+	string argh = "AAAARRRGH, oh god how do they do that? As you know I'm Cap'n Slap. I... won't explain how I got that title. Just know I have several Space-Restraining orders. Anyway, welcome to the Space Citadel. I'll be watching your progress from the Space-Television. Good Luck!";
+	typeIt(argh, 45, 1);
+	cout << "_PRESS ENTER TO....... ENTER THE DUNGEON_" << endl;
+
+	return 0;
+}
+
+//function for making our text lines type out and make it more enjoyable.
+void delay(int x)
+{
+	std::this_thread::sleep_for(std::chrono::milliseconds(x));
+}
+
+// Write out char by char with a delay
+//example: typeIt("Hello world", 1000, 1;
+//                ^ string       ^ms   ^add endline
+void typeIt(std::string it, int x, bool y)
+{
+	for (auto i = 0; i < it.length(); i++)
+	{
+
+		std::cout << it[i];
+		delay(x);
+	}
+	if (y == true)
+		std::cout << std::endl;
+}
