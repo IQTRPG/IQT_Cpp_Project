0b4f9d3fdd7ac12bbade3a55521254cf42a13cee
Elias Perez <37880438+Elionidas@users.noreply.github.com>
2018-08-27 11:23:41 -0500

    updated enemies.h

--- Space Citadel Classes/Enemies.h
+++ Space Citadel Classes/Enemies.h
@@ -15,6 +15,8 @@ public:
 	int CAR = 0; //charisma, affects shop prices maybe?
 	int INT = 0; //intelligence, affects mp and magic damage
 	int LUCK = 0; //luck, affects dodge chance
+	int xPos = 0; //position
+	int yPos = 0; //position
 
 	//display the enemy's stats when requested
 	void display_stats() {
@@ -36,5 +38,52 @@ public:
 	void attack()
 	{
 		//initiate battle code
+	}
+};
+
+class Boss {
+public:
+	int HP = 0; //health points, will be assigned by class
+	int MP = 0; //magic points, assigned by class and affects how often you can use spells
+	int DEF = 0; //defense, can be changed by armor value
+	int armor = 0; //changes based on armor equipped
+	int AGI = 0; //agility, affected by armor possibly, affects crit chance
+	int STR = 0; //strength, affected by weapons, affects damage
+	int CAR = 0; //charisma, affects shop prices maybe?
+	int INT = 0; //intelligence, affects mp and magic damage
+	int LUCK = 0; //luck, affects dodge chance
+	int xPos = 0;
+	int yPos = 0;
+
+	//display the enemy's stats when requested
+	void display_stats() {
+		cout << "Health: " << HP << endl;
+		cout << "Mana: " << MP << endl;
+		cout << "Defense: " << DEF << endl;
+		cout << "Agility: " << AGI << endl;
+		cout << "Strength: " << STR << endl;
+		cout << "Charisma: " << CAR << endl;
+		cout << "Intelligence: " << INT << endl;
+		cout << "Luck: " << LUCK << endl;
+	}
+
+	//allows the character to move
+	void position(int xpos, int ypos) {
+		xPos = xpos;
+		yPos = ypos;
+	}
+
+	void attack()
+	{
+		//initiate battle code
+	}
+
+	void Ability() {
+		//add ability code. ties in with status check id assume
+		
+	}
+
+	void Summon() {
+		//not really a summon, it more just makes sure we add minions to the boss fight
 	}
 };
